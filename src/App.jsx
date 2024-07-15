bc0b6e8fdd6ff15fe8d99068c57b71242faae2d6
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-07-15 22:05:27 +0530

    Navigation bar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Navbar from "./components/Navbar";
+import Highlights from "./components/Highlights";
+import Hero from "./components/Hero";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="bg-black">
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Highlights></Highlights>
+    </main>
+  );
 };
 
 export default App;
